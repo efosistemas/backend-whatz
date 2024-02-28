890e049ba3e40d09149c7ec22768a288f593c03f
Eduardo <eduardof@fmb.unesp.br>
2024-02-28 15:33:22 -0300

    commit 1003

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express'
 import { UserController } from './controllers/UserController'
 import { TodoController } from './controllers/TodoController'
+import { authMiddleware } from './middlewares/authMiddleware'
 
 const routes = Router()
 
 routes.post('/login', new UserController().login)
 routes.post('/user', new UserController().create)
-routes.get('/users', new UserController().users)
+routes.get('/users', authMiddleware, new UserController().users)
+routes.get('/profile', authMiddleware, new UserController().getProfile)
+
 
 routes.post('/todo', new TodoController().create)
 routes.get('/todos', new TodoController().todos)
