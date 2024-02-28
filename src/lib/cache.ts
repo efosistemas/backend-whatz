53a68670f178070b0d33944b5aaecdd46245b31f
Eduardo <eduardof@fmb.unesp.br>
2024-02-28 16:23:22 -0300

    commit 1005

---
+++ src/lib/cache.ts
@@ -0,0 +1,8 @@
+import IOredis from 'ioredis'
+
+const redis = new IOredis({
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+    password: process.env.REDIS_PASSWORD
+});
+export default redis;
