9135bf066a3aa9e70270e95063c418ef4e2fd161
Eduardo <eduardof@fmb.unesp.br>
2024-02-29 14:05:35 -0300

    commit 2010

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express'
 import { UserController } from './controllers/UserController'
-import { MensagemController } from './controllers/MensagemController'
 import { authMiddleware } from './middlewares/authMiddleware'
 
 const routes = Router()
@@ -10,7 +9,6 @@ routes.post('/user', new UserController().create)
 routes.get('/users', authMiddleware, new UserController().users)
 routes.get('/profile', authMiddleware, new UserController().getProfile)
 
-routes.get('/mensagens', new MensagemController().mensagens)
 routes.post('/mensagem', new UserController().create)
 
 export default routes
